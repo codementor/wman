823cbededa120df66a21d8f6b3a45cfaceab18ee
Ken Sipe <kensipe@gmail.com>
2020-06-04 19:05:32 -0500

    lab 2 reverse function from package
    Signed-off-by: Ken Sipe <kensipe@gmail.com>

--- cmd/wman/main.go
+++ cmd/wman/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+
+
 )
+import 	"github.com/codementor/wman/pkg/string"
 
 func main() {
 	name := os.Args[1]
-	fmt.Printf("hello %s!", name)
+	fmt.Printf("hello %s, your name backward is %q", name, string.Reverse(name))
 }
